a7fa2ecd6f523b738c8a1ed3008771c9be235307
Eric Malone <ejmalone@gmail.com>
2017-02-13 19:56:20 +0000

    updating ux to include info links

--- src/components/email_deduplicator.js
+++ src/components/email_deduplicator.js
@@ -9,7 +9,7 @@ class EmailDeduplicator extends React.Component {
 
    render() {
       return (
-         <div className="container">
+         <div className="row">
             
             <div className="col-md-4">
                <InputList />
